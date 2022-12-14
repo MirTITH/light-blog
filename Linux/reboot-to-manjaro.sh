6288c39a1c5ea1dea9d357fc4b4899628e010baa
XY <1023515576@qq.com>
2022-12-14 13:56:17 +0800

    new file:   Linux/reboot-to-manjaro.sh

---
+++ Linux/reboot-to-manjaro.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+search_result=$(efibootmgr | grep Manjaro)
+boot_id=${search_result:4:4}
+
+# echo --------------------
+# echo $search_result
+# echo --------------------
+# echo
+# echo 若 ubuntu 的启动 id 为 $boot_id, 按 Enter
+# echo 否则请 ctrl+c 然后输入： sudo efibootmgr --bootnext xxxx
+# echo 
+# read -p Continue?
+
+sudo efibootmgr --bootnext $boot_id
+sudo reboot
