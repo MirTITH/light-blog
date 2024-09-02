21694808d2c6227985ccb8f9851b8b32e8b224b7
Yang XIE <1023515576@qq.com>
2024-09-02 16:54:06 +0800

    Update docker: ros-humble

--- Linux/docker/ros_humble/create_container.sh
+++ Linux/docker/ros_humble/create_container.sh
@@ -20,6 +20,27 @@ workspace_dir=$(dirname "$script_dir") # 工作空间的目录，即 script_dir 
 echo "IMAGE_NAME: $IMAGE_NAME"
 echo "CONTAINER_NAME: $CONTAINER_NAME"
 
+check_and_create_file() {
+    local file_path="$1"
+
+    if [ ! -f "$file_path" ]; then
+        touch "$file_path"
+        echo "File '$file_path' does not exist, created."
+    fi
+}
+
+check_and_create_directory() {
+    local dir_path="$1"
+
+    if [ ! -d "$dir_path" ]; then
+        mkdir -p "$dir_path"
+        echo "Directory '$dir_path' does not exist, created."
+    fi
+}
+
+check_and_create_file $HOME/.gitconfig
+check_and_create_directory $HOME/.ssh
+
 docker run -d --name $CONTAINER_NAME --user $USER_NAME \
     --network=host --ipc=host \
     --privileged \
@@ -27,6 +48,7 @@ docker run -d --name $CONTAINER_NAME --user $USER_NAME \
     --runtime=nvidia --gpus all --env NVIDIA_DRIVER_CAPABILITIES=all \
     -v $XDG_RUNTIME_DIR/pulse:/tmp/pulse -e PULSE_SERVER=unix:/tmp/pulse/native -v $HOME/.config/pulse/cookie:/home/$USER_NAME/.config/pulse/cookie \
     -v $HOME/.ssh:/home/$USER_NAME/.ssh \
+    -v $HOME/.gitconfig:/home/$USER_NAME/.gitconfig \
     -v $HOME/Documents:/home/$USER_NAME/Documents \
     -v $HOME/Downloads:/home/$USER_NAME/Downloads \
     -v $script_dir/.bash_aliases:/home/$USER_NAME/.bash_aliases \
