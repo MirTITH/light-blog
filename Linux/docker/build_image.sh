4f6577efe8ebb4c53e91d7eefa8712cfb7e489ac
Yang XIE <1023515576@qq.com>
2025-01-03 13:04:52 +0800

    Update docker: 不用复制 mirrors 了

--- Linux/docker/build_image.sh
+++ Linux/docker/build_image.sh
@@ -6,36 +6,40 @@ script_path=$(readlink -f "$0")        # 脚本文件的绝对路径
 script_dir=$(dirname "$script_path")   # 脚本文件的目录
 
 print_usage() {
-    echo "Usage: $0 Dockerfile_FOLDER [IMAGE_NAME]"
+    echo "Usage: $0 DOCKERFILE_FOLDER IMAGE_NAME"
+    echo "Example: $0 ros-humble my-ros-humble"
 }
 
-Dockerfile_FOLDER=$1
-if [ -z "$Dockerfile_FOLDER" ]; then
-    echo "Dockerfile_FOLDER not specified"
+# Docker file folder
+DOCKERFILE_FOLDER=$1
+if [ -z "$DOCKERFILE_FOLDER" ]; then
+    echo "DOCKERFILE_FOLDER not specified"
     print_usage
     exit 1
 fi
-BUILD_DIR=$script_dir/$Dockerfile_FOLDER
+DOCKERFILE_PATH=$script_dir/$DOCKERFILE_FOLDER/Dockerfile
 
+# Image name
 IMAGE_NAME=$2
 if [ -z "$IMAGE_NAME" ]; then
-    IMAGE_NAME=$Dockerfile_FOLDER
-    echo "IMAGE_NAME not specified, use default: $IMAGE_NAME"
+    echo "IMAGE_NAME not specified"
     print_usage
-    read -p "Press enter to continue"
+    exit 1
 fi
 echo "IMAGE_NAME: $IMAGE_NAME"
 
+# Build the image
 DOCKER_ARGS=(
-    build -t $IMAGE_NAME $BUILD_DIR
+    build -f $DOCKERFILE_PATH -t $IMAGE_NAME $script_dir
+    --build-arg DOCKER_FILE_FOLDER=$DOCKERFILE_FOLDER
     # --build-arg "http_proxy=http://localhost:7890"
     # --build-arg "https_proxy=http://localhost:7890"
     # --build-arg "all_proxy=socks5://localhost:7890"
     # --network host
 )
 
-cp -r $script_dir/mirrors $BUILD_DIR
+# echo "The following command will be run:"
+# echo "docker ${DOCKER_ARGS[@]}"
+# read -p "Press Enter to continue..."
 
-docker "${DOCKER_ARGS[@]}"
-
-rm -rf $BUILD_DIR/mirrors
+docker ${DOCKER_ARGS[@]}
